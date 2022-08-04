2a17b739f81d96364238f2ed137e1608d5f6b3a0
SoupSedan <104839304+SoupSedan@users.noreply.github.com>
2022-08-03 18:18:44 -1000

    Create mod.cpp

---
+++ @SoupsMedicalSuite/Addons/mod.cpp
@@ -0,0 +1,8 @@
+name="Soup's Medical Suite"
+picture="\Soup_Core\banner.paa";
+logo="\Soup_Core\smalllogo.paa";
+logoOver="\Soup_Core\logo.paa";
+actionName="GitHub"
+action="https://github.com/SoupSedan/soups-medical-suite"
+overview="Thank you to the stitch bitches everywhere";
+logoSmall="\Soup_Core\smalllogo.paa";
